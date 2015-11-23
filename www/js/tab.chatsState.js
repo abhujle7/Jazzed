94c706efbe1320da54ed156b912a2b7b6d663701
Anup Bhujle <abhujle7@gmail.com>
2015-11-23 14:56:41 -0500

    refactored states

---
+++ www/js/tab.chatsState.js
@@ -0,0 +1,14 @@
+app.config(function($stateProvider) {
+
+  $stateProvider
+
+  .state('tab.chats', {
+    url: '/chats',
+    views: {
+      'tab-chats': {
+        templateUrl: 'templates/tab-chats.html',
+        controller: 'ChatsCtrl'
+      }
+    }
+  })
+});
