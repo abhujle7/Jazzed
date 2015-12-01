cda8e479e6d3f7eee4d640b0a91229463db3062e
Anup Bhujle <abhujle7@gmail.com>
2015-11-30 20:31:09 -0500

    messages persist

--- www/js/rooms/rooms.factory.js
+++ www/js/rooms/rooms.factory.js
@@ -4,8 +4,10 @@ app.factory('RoomsFactory', function($firebaseArray, $firebaseAuth, AuthFactory)
   var roomsArr = $firebaseArray(roomsRef);
   // var roomsArr = $firebaseArray(ref.child('groups'));
   // var roomsRef = ref.child('groups')
+  var currUserObj = AuthFactory.getCurrentUser();
   var currUser = AuthFactory.getCurrentUser().uid
   var currUserRooms = new Firebase('https://boiling-fire-3161.firebaseio.com/users/' + currUser + '/groups')
+  var userRef = new Firebase('https://boiling-fire-3161.firebaseio.com/users/' + currUser)
 
   return {
 
@@ -18,18 +20,19 @@ app.factory('RoomsFactory', function($firebaseArray, $firebaseAuth, AuthFactory)
     },
 
     add: function(roomObj) {
-      console.log('this is roomobj in fac', roomObj)
       return roomsArr.$add(roomObj)
       .then(function (ref) {
-        console.log('this is the ref', ref)
         var id = ref.key();
-        console.log('this is the id in fac', id)
         currUserRooms.child(id).set({name: roomObj.name})
-        $firebaseArray(roomsRef.child(id).child('members')).$add(currUser) //also want to add all selected users
+        //removed validation of user_id = user uid
+        $firebaseArray(roomsRef.child(id).child('members')).$add({
+          id: currUser
+        }).then(function (data) {
+          console.log('this the data', data)
+        })
+     
         return id;
       })
-        // console.log('added record with id' + id);
-        // return rooms.$indexFor(id);
         //state.go to chat detail with new id
 
     }
