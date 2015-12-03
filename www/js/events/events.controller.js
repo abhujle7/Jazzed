b584722a1ccc82599e395b66d3cc46bcd9d937fa
Anup Bhujle <abhujle7@gmail.com>
2015-12-03 12:52:53 -0500

    saving

--- www/js/events/events.controller.js
+++ www/js/events/events.controller.js
@@ -1,4 +1,4 @@
-app.controller('EventsCtrl', function($scope, $state, EventFactory, RoomsFactory) {
+app.controller('EventsCtrl', function($scope, $state, EventFactory, RoomsFactory, $ionicHistory) {
 
 	var promise = EventFactory.all();
 	$scope.rooms = RoomsFactory.all();
@@ -23,6 +23,10 @@ app.controller('EventsCtrl', function($scope, $state, EventFactory, RoomsFactory
 			EventFactory.addCustomEvent($scope.data.name, $scope.data.time, $scope.data.location, $scope.data.locationName, $scope.data.group_id);
 		else
 			EventFactory.addFixedEvent($scope.data.name, $scope.data.time, $scope.data.location, $scope.data.locationName);
+	}
+
+	$scope.goBack = function () {
+		$ionicHistory.goBack();
 	}
  })
 
