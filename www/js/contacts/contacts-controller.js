6f0fda56d9bc85b3b29e684f661eee0596f6bcbe
Anup Bhujle <abhujle7@gmail.com>
2015-12-02 23:15:57 -0500

    unformatted contacts show

--- www/js/contacts/contacts-controller.js
+++ www/js/contacts/contacts-controller.js
@@ -1,9 +1,45 @@
-app.controller('ContactsCtrl', function($scope, AuthFactory, $firebaseArray, $cordovaContacts) {
-	var ref = new Firebase('https://boiling-fire-3161.firebaseio.com/users/' + AuthFactory.getCurrentUser().uid + '/contacts')
-	$scope.contacts = []
-	$scope.syncContacts = function() {
-		$cordovaContacts.find().then(function(allContacts) {
-			$scope.contacts = allContacts || [1,2,3]
-		})
-	}
-})
+app.controller('ContactsCtrl', function($scope, $cordovaContacts, AuthFactory, $firebaseArray){
+		
+	$scope.contacts = ['here we go'];
+		
+		document.addEventListener("deviceready", onDeviceReady, false)
+
+		
+		function onDeviceReady () {
+			function onSuccess(contacts) {
+			    alert('Found ' + contacts.length + ' contacts.');
+			    for (var i = 0; i<contacts.length; i++) {
+			    	$scope.contacts.push(contacts[i]);
+			    }
+			};
+
+			function onError(contactError) {
+			    alert('onError!');
+			};
+
+			var options      = new ContactFindOptions();
+			options.filter   = "";
+			options.multiple = true;
+			options.desiredFields = [navigator.contacts.fieldType.id];
+			options.hasPhoneNumber = true; //android only
+			var fields       = [navigator.contacts.fieldType.displayName, navigator.contacts.fieldType.name];
+			navigator.contacts.find(fields, onSuccess, onError, options)
+     		
+     		// return $scope.contacts;
+     	}
+	
+		$scope.getContacts = function () {
+			return $scope.contacts;
+		}
+
+
+     });
+
+
+
+
+
+
+
+
+
