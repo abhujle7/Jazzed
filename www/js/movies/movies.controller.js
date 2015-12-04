f48ff17d99c06889adca6c1d5e9a5e89b6a7f066
David Kim <Davkime@gmail.com>
2015-12-04 16:37:19 -0500

    added time

--- www/js/movies/movies.controller.js
+++ www/js/movies/movies.controller.js
@@ -1,10 +1,11 @@
 app.controller('MoviesCtrl', function($scope, $state, MoviesFactory) {
-	$scope.search = function(zip) {
+	$scope.search = function(data) {
+		data.date = moment(data.date)
+		var now = moment(Date.now());
+		var movieDate = data.date.diff(now, "days") + 1;
 		return MoviesFactory.get(zip)
 		 	.then(function(movies) {
-		 		console.log(movies)
 				$state.go('tab.mvresults')
 		 	})
 	}
-
 })
