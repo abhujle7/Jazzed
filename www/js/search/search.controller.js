b7c27bbed5517df5a932db82d5ea793d81eeee35
Silvia C <silviachen917@gmail.com>
2015-12-04 10:54:57 -0500

    sporting api

--- www/js/search/search.controller.js
+++ www/js/search/search.controller.js
@@ -1,6 +1,10 @@
 app.controller('SearchCtrl', function($scope, SportsFactory, $state) {
 
 	$scope.search = function(eventInfo) {
+		if (eventInfo.date) {
+		eventInfo.date = moment(eventInfo.date).format('YYYY-MM-DD')
+		}
+
 		 return SportsFactory.get(eventInfo)
 		 	.then(function(data) {
 				$state.go('tab.results')
--- www/js/sports.factory.js
+++ www/js/sports.factory.js
@@ -5,11 +5,17 @@ app.factory('SportsFactory', function($http) {
 			var baseUrl = 'http://api.seatgeek.com/2/events?'
 			if (info.performer) {
 				var person = info.performer.toLowerCase().split(' ').join('-');
-				baseUrl += '&performers.slug=' + person
+				baseUrl += '&q=' + person
+				console.log(baseUrl)
+			}
+			if (info.date) {
+				console.log(info.date)
+				baseUrl += '&datetime_local.gte=' + info.date
 			}
 			if (info.state) {
 				baseUrl += '&venue.state=' + info.state
 			}
+
 			return $http.get(baseUrl)
 				.then(function(response) {
 					data = response.data.events
