b3560ba1293d88b7f6039c1e03e4735d888c16e0
Anup Bhujle <abhujle7@gmail.com>
2015-12-04 16:34:49 -0500

    poll intervals

--- www/js/polls/polls.controller.js
+++ www/js/polls/polls.controller.js
@@ -1,9 +1,11 @@
-app.controller('PollsCtrl', function($scope, $state, EventFactory, RoomsFactory, $ionicHistory, PollsFactory, eventDetails) {
+app.controller('PollsCtrl', function($scope, $state, EventFactory, RoomsFactory, $ionicHistory, PollsFactory, eventDetails, $interval, $firebase) {
 
 	$scope.rooms = RoomsFactory.all();
 	$scope.polls = PollsFactory.all();
 	$scope.event = eventDetails;
-	
+	var pollsRef = new Firebase('https://boiling-fire-3161.firebaseio.com/polls/');
+	// var polls = $firebaseArray(ref.child('polls'));
+
 	$scope.data = {
 		location: null,  //if event has location, display and void this field in poll
 		expiration: {
@@ -29,4 +31,36 @@ app.controller('PollsCtrl', function($scope, $state, EventFactory, RoomsFactory,
 		PollsFactory.addPoll($scope.data)
 	}
 	
+	$scope.eventLocation = function () {
+		if ($scope.event.location) {
+			return true;
+		}
+		return false;
+	}
+
+
+
+	$interval(function(){
+	      
+		pollsRef.on("value", function (snapshot) {
+			snapshot.forEach(function(groupSnapshot) {
+				var group = groupSnapshot.val()
+				var groupid = groupSnapshot.key()
+				for (var poll in group) {
+					// console.log('this is date', group[poll].expiration.date)
+					// console.log('this is date', group[poll].expiration.time)
+					// console.log('this is moment', moment().unix());
+					
+					var combinedDate = group[poll].expiration.date + group[poll].expiration.time
+					// console.log('this is combined', combinedDate)
+					if (moment().unix() >= combinedDate) {
+						pollsRef.child(groupid).child(poll).live = false;
+						// pollsRefgroup[poll].live = false;
+					}
+				}
+			})
+		})
+
+	}, 1000);
+
  })
--- www/js/polls/polls.factory.js
+++ www/js/polls/polls.factory.js
@@ -2,7 +2,7 @@ app.factory('PollsFactory', function($state, $firebase, $firebaseArray, $ionicHi
 	
   var selectedRoomId = ChatFactory.getRoomId();
 	var ref = new Firebase('https://boiling-fire-3161.firebaseio.com');
-	var polls = $firebaseArray(ref.child('poll').child(selectedRoomId));
+	var polls = $firebaseArray(ref.child('polls').child(selectedRoomId));
 	var currentUser = AuthFactory.getCurrentUser();
   var user = AuthFactory.getCurrentUser().uid
   var userRef = new Firebase('https://boiling-fire-3161.firebaseio.com/users/' + user)
@@ -17,6 +17,10 @@ app.factory('PollsFactory', function($state, $firebase, $firebaseArray, $ionicHi
       console.log('this is polldata', pollData)
       pollData.timestamp = Firebase.ServerValue.TIMESTAMP;
       pollData.creator = user;
+      pollData.expiration.date = moment(pollData.expiration.date).unix();
+      pollData.expiration.time = moment(pollData.expiration.time).unix();
+      pollData.live = true;
+      console.log('this is polldata modified', pollData)
       polls.$add(pollData)
       .then(function(data) {
         // $ionicHistory.goBack(); //goes back to previous view
